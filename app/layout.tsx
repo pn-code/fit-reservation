4f8f03485dd2aa6ed00ea77b494a25968da19711
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-27 18:36:22 -0700

    Initial commit with Next.js 13 & Tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ["latin"],
+  variable: "--font-roboto"
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={`${roboto.className}`}>{children}</body>
+    </html>
+  )
+}
