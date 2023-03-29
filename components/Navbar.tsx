ca625db60477f333f71adc4a649a4f7ef6c2a787
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-29 12:19:17 -0700

    Updating Navbar with actual pages and links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-    const navigationLinks = ["home", "calculator", "tracker", "profile"];
     return (
         <nav className="flex justify-between my-6">
             <h1>FitHeroes</h1>
             <ul className="flex gap-4">
-                {navigationLinks.map((link) => (
-                    <Link className="hover:underline" href={`/${link}`}>{link}</Link>
-                ))}
+                    <Link className="hover:underline" href="/">Home</Link>
+                    <Link className="hover:underline" href="/calculator">Calculator</Link>
+                    <Link className="hover:underline" href="/tracker">Tracker</Link>
+                    <Link className="hover:underline" href="/profile">Profile</Link>
             </ul>
         </nav>
     );
