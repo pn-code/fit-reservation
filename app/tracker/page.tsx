683fa2c1b278c4647571e08a7f9480a456d3615e
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-29 14:07:04 -0700

    Initial build of tracker page

---
+++ app/tracker/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Tracker = () => {
+    return (
+        <div className="mt-[6%] w-full bg-[#f3f3f3] px-4 py-6 rounded-md flex flex-col gap-4 shadow-md">
+            <header>
+                <h1 className="text-3xl font-bold border-b-2 border-b-[#F15B2A]">
+                    Track Your Progress
+                </h1>
+            </header>
+
+            {/* Nutrition */}
+            <section className="bg-gray-100 text-black rounded-sm">
+                <header>
+                    <h2 className="text-lg font-semibold underline mb-2 ">
+                        Nutrition
+                    </h2>
+                    <div className="flex flex-col gap-4 md:flex-row bg-gray-100 py-2 md:items-center">
+                        <label htmlFor="">Item Name</label>
+                        <input type="text" />
+                        <label htmlFor="">Calories</label>
+                        <input type="text" />
+                        <button className="bg-[#05204A] text-[#fafafa] px-4 py-2 rounded-md">
+                            Add Item
+                        </button>
+                    </div>
+                </header>
+            </section>
+
+            <h2 className="text-lg font-semibold underline">Exercise</h2>
+            <section></section>
+        </div>
+    );
+};
+
+export default Tracker;
