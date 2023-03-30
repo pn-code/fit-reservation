024ad4bec9c87381a455d10473e20fd4ab4de45c
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-29 17:23:26 -0700

    Adding styles and logo to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
             <head />
-            <body className={`${roboto.className} mx-6`}>
+            <body className={`${roboto.className}`}>
                 <Navbar />
                 {children}
             </body>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between my-6">
-            <h1>FitHeroes</h1>
+        <nav className="bg-blue-900 text-white py-6 px-6 flex justify-between items-center fixed w-full top-0 left-0 z-50">
+            <Image
+                className="rounded-sm cursor-pointer"
+                src="/assets/logo.png"
+                alt="FitHeroes Logo"
+                width={160}
+                height={100}
+            />
             <ul className="flex gap-4">
-                    <Link className="hover:underline" href="/">Home</Link>
-                    <Link className="hover:underline" href="/calculator">Calculator</Link>
-                    <Link className="hover:underline" href="/tracker">Tracker</Link>
-                    <Link className="hover:underline" href="/profile">Profile</Link>
+                <Link className="hover:underline font-medium" href="/">
+                    Home
+                </Link>
+                <Link
+                    className="hover:underline font-medium"
+                    href="/calculator"
+                >
+                    Calculator
+                </Link>
+                <Link className="hover:underline font-medium" href="/tracker">
+                    Tracker
+                </Link>
+                <Link className="hover:underline font-medium" href="/profile">
+                    Profile
+                </Link>
             </ul>
         </nav>
     );
