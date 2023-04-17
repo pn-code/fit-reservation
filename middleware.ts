c7896940ee4016e8ebf6daab039b41bdc75fbc2b
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-17 14:26:59 -0700

    Handling rerouting if user is not authenticated

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,33 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
- 
+
+// Setting up public paths
+const publicPaths = ["/", "/register*", "/login*"];
+
+// Function to determine if current url matches our public paths...
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+// How to deal with authenticated users and nonauthenticated users...
 export default withClerkMiddleware((req: NextRequest) => {
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(req);
+ 
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+ 
+    const signInUrl = new URL("/login", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
   return NextResponse.next();
 });
  
