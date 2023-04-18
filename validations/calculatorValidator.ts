854cc5491f2c6628e44db78c6b269e21fe9d7bb6
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-18 12:50:53 -0700

    Initial build of calculator validators

---
+++ validations/calculatorValidator.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const calculatorValidator = z.object({
+	age: z.number().gt(4),
+	gender: z.literal("male") || z.literal("female"),
+	height: z.object({
+		feet: z.number().lt(9).gt(2),
+		inches: z.number().lt(12),
+	}),
+	weight: z.number().lt(1000).gt(60),
+	activity: z.number().gt(0.5).lt(2),
+	plan: z.number(),
+});
+
+export const calorieValidator = z.number();
