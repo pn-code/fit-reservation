f65385340b2031b60a8d3917c10d588e6f62d7cb
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-18 20:45:12 -0700

    Removed GET from route

--- app/api/calorie_goal/route.ts
+++ app/api/calorie_goal/route.ts
@@ -1,48 +1,29 @@
-import { NextResponse } from "next/server";
+import { currentUser } from "@clerk/nextjs/dist/app-beta";
 import { prisma } from "../../../lib/client";
-import { currentUser } from "@clerk/nextjs/app-beta";
-
-export async function GET(req: Request) {
-	try {
-		const user = await currentUser();
-
-		if (!user) throw Error;
-
-		const calorieGoal = await prisma.calorieGoal.findFirst({
-			where: {
-				userId: user.id
-			}
-		})
-
-		return NextResponse.json(calorieGoal)
-
-	} catch{
-		return NextResponse.json({ error: "Ran into an error..." });
-	}
-}
+import { NextResponse } from "next/server";
 
 export async function PUT(req: Request) {
-	try {
-		const user = await currentUser();
-		const { goal } = await req.json();
+    try {
+        const user = await currentUser();
+        const { goal } = await req.json();
 
-		if (!user || !goal) throw Error;
+        if (!user || !goal) throw Error;
 
-		const updateCalorieGoal = await prisma.calorieGoal.upsert({
-			where: {
-				userId: user.id,
-			},
-			create: {
-				userId: user.id,
-				goal,
-			},
-			update: {
-				goal,
-			},
-		});
+        const updateCalorieGoal = await prisma.calorieGoal.upsert({
+            where: {
+                userId: user.id,
+            },
+            create: {
+                userId: user.id,
+                goal,
+            },
+            update: {
+                goal,
+            },
+        });
 
-		return NextResponse.json(updateCalorieGoal);
-	} catch (error) {
-		return NextResponse.json({ error: "Ran into an error..." });
-	}
+        return NextResponse.json(updateCalorieGoal);
+    } catch (error) {
+        console.error(error);
+    }
 }
