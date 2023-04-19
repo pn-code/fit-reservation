658dd8125cc087421da354bd633843176537ec07
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-19 14:27:28 -0700

    Added phys. activity section of tracker overview

--- app/tracker/overview/page.tsx
+++ app/tracker/overview/page.tsx
@@ -1,20 +1,46 @@
 import TrackerHeader from "../../../components/TrackerHeader";
 import MacroChart from "../../../components/MacroChart";
+import LineDivider from "../../../components/LineDivider";
 
 const Tracker = () => {
 	return (
-		<main className="w-full h-[calc(100vh-96px)] mt-24 bg-[#f3f3f3] px-4 py-6 rounded-md flex flex-col gap-4 shadow-md">
+		<main className="w-full h-full mt-24 bg-[#f3f3f3] px-4 py-6 rounded-md flex flex-col gap-4 shadow-md">
 			<TrackerHeader title={"Tracker Overview"} />
 
 			<section className="flex flex-col items-center w-full">
 				<h2 className="text-2xl font-semibold text-slate-900 text-center">
 					April 19, 2023
 				</h2>
-				<div className="w-full border-b-2 border-b-orange-600 pb-4"></div>
+				<LineDivider />
 			</section>
 
+			{/* Physical Activity Section */}
 			<section className="flex flex-col items-center gap-4">
-				<h2 className="text-2xl font-semibold text-indigo-700">Your Nutrition</h2>
+				<h2 className="text-2xl font-semibold text-indigo-700">
+					Your Physical Activity
+				</h2>
+
+				<article className="flex flex-col items-center">
+					<h3 className="text-xl text-slate-900 font-bold">
+						Activity Goal
+					</h3>
+					<span className="text-lg text-slate-800">90 mins</span>
+				</article>
+
+				<article className="flex flex-col items-center">
+					<h3 className="text-xl text-slate-900 font-bold">
+						Total Activity
+					</h3>
+					<span className="text-lg text-slate-800">96 mins</span>
+				</article>
+				<LineDivider />
+			</section>
+
+			{/* Nutrition Section */}
+			<section className="flex flex-col items-center gap-4">
+				<h2 className="text-2xl font-semibold text-indigo-700">
+					Your Nutrition
+				</h2>
 				<article className="flex flex-col items-center">
 					<h3 className="text-xl text-slate-900 font-bold">
 						Total Calories
@@ -22,7 +48,7 @@ const Tracker = () => {
 					<span className="text-lg text-slate-800">1800 kcal</span>
 				</article>
 
-				<article className="flex flex-col items-center">
+				<article className="flex flex-col items-center w-[80%]">
 					<h3 className="text-xl text-slate-900 font-bold">
 						Macronutrients
 					</h3>
---
+++ components/LineDivider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LineDivider() {
+  return (
+    <div className="w-full border-b-2 border-b-orange-600 pb-4"/>
+  )
+}
+
+export default LineDivider
