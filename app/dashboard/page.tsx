f164e3df09df4abf9c316f549c8dc1a400c92e64
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-19 14:17:15 -0700

    Finished out nutrition section of overview

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import MacroChart from "../../components/MacroChart";
 
 function DashboardPage() {
-  return (
-    <main className="h-[100vh-200px] w-full mt-32 mx-4">
-      <header>
-        <h1>Dashboard</h1>
-      </header>
-    </main>
-  )
+	return (
+		<main className="h-[100vh-200px] w-full mt-32 mx-4">
+			<header>
+				<h1>Dashboard</h1>
+			</header>
+			<main>
+				<MacroChart />
+			</main>
+		</main>
+	);
 }
 
 export default DashboardPage;
--- app/tracker/overview/page.tsx
+++ app/tracker/overview/page.tsx
@@ -3,13 +3,32 @@ import MacroChart from "../../../components/MacroChart";
 
 const Tracker = () => {
 	return (
-		<main className="w-full h-full mt-24 bg-[#f3f3f3] px-4 py-6 rounded-md flex flex-col gap-4 shadow-md">
+		<main className="w-full h-[calc(100vh-96px)] mt-24 bg-[#f3f3f3] px-4 py-6 rounded-md flex flex-col gap-4 shadow-md">
 			<TrackerHeader title={"Tracker Overview"} />
 
-			<section>
-				<MacroChart carbs={300} fats={60} protein={200} />
+			<section className="flex flex-col items-center w-full">
+				<h2 className="text-2xl font-semibold text-slate-900 text-center">
+					April 19, 2023
+				</h2>
+				<div className="w-full border-b-2 border-b-orange-600 pb-4"></div>
 			</section>
-			
+
+			<section className="flex flex-col items-center gap-4">
+				<h2 className="text-2xl font-semibold text-indigo-700">Your Nutrition</h2>
+				<article className="flex flex-col items-center">
+					<h3 className="text-xl text-slate-900 font-bold">
+						Total Calories
+					</h3>
+					<span className="text-lg text-slate-800">1800 kcal</span>
+				</article>
+
+				<article className="flex flex-col items-center">
+					<h3 className="text-xl text-slate-900 font-bold">
+						Macronutrients
+					</h3>
+					<MacroChart carbs={300} fats={60} protein={200} />
+				</article>
+			</section>
 		</main>
 	);
 };
