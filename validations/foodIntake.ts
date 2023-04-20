9fda5951f72bd579102034dd1f01eacb69938d5f
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-20 16:57:59 -0700

    Increased max char for name

--- validations/foodIntake.ts
+++ validations/foodIntake.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const foodIntakeSchema = z.object({
-    name: z.string().min(1).max(30),
+    name: z.string().min(1).max(60),
     calories: z.number().int().lte(9999),
     carbs: z.number().lte(500),
     fats: z.number().lte(500),
