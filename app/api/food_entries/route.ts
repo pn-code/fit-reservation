68fc02e91778ac95f1e4ff0d1f2ad08f65d45204
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-20 20:29:50 -0700

    Added delete endpoint

--- app/api/food_entries/route.ts
+++ app/api/food_entries/route.ts
@@ -3,26 +3,55 @@ import { prisma } from "../../../lib/client";
 import { currentUser } from "@clerk/nextjs/app-beta";
 
 export async function POST(req: Request) {
-    try {
-        const user = await currentUser();
+	try {
+		const user = await currentUser();
 
-        if (user) {
-            const res = await req.json();
-    
-            const newFoodEntry = await prisma.foodEntry.create({
-                data: {
-                    name: res.name,
-                    calories: res.calories,
-                    carbs: res.carbs,
-                    fats: res.fats,
-                    protein: res.protein,
-                    userId: user.id
-                },
-            });
+		if (user) {
+			const res = await req.json();
 
-            return NextResponse.json(newFoodEntry);
-        }
-    } catch (error) {
-        return console.error(error)
-    }
+			const newFoodEntry = await prisma.foodEntry.create({
+				data: {
+					name: res.name,
+					calories: res.calories,
+					carbs: res.carbs,
+					fats: res.fats,
+					protein: res.protein,
+					userId: user.id,
+				},
+			});
+
+			return NextResponse.json(newFoodEntry);
+		}
+	} catch (error) {
+		return console.error(error);
+	}
+}
+
+export async function DELETE(req: Request) {
+	try {
+		const user = await currentUser();
+		const res = await req.json();
+
+		if (!user) throw Error("This action is forbidden.");
+
+		// Check to see if this entry is this user's
+		const foundEntryOwner = await prisma.foodEntry.findFirst({
+			where: { id: res.id },
+			select: {
+				userId: true,
+			},
+		});
+
+		if (foundEntryOwner?.userId != user.id) return NextResponse.error();
+
+		const foodEntry = await prisma.foodEntry.delete({
+			where: {
+				id: res.id,
+			},
+		});
+
+		return NextResponse.json(foodEntry);
+	} catch (error) {
+		console.error(error);
+	}
 }
