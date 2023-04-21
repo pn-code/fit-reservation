a31fdcbfd8569af7a790cd15594df9242936ed19
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-21 13:34:48 -0700

    Added styles for journals on larger devices

--- app/tracker/overview/page.tsx
+++ app/tracker/overview/page.tsx
@@ -75,84 +75,106 @@ const TrackerOverview = async () => {
 						<h3 className="text-xl text-slate-900 font-bold">
 							Macronutrients
 						</h3>
-						<MacroChart
-							carbs={totalCarbs || 0}
-							fats={totalFats || 0}
-							protein={totalProtein || 0}
-						/>
+						{totalCarbs != 0 &&
+						totalFats != 0 &&
+						totalProtein != 0 ? (
+							<MacroChart
+								carbs={totalCarbs || 0}
+								fats={totalFats || 0}
+								protein={totalProtein || 0}
+							/>
+						) : (
+							<p className="text-slate-800">No Stats Available</p>
+						)}
 					</article>
 				</section>
 			</section>
 
-			<section className="flex flex-col items-center gap-4 mt-5">
-				<LineDivider />
-				<h2 className="text-2xl font-semibold text-indigo-700">
-					Nutrition Journal
-				</h2>
-				<table className="w-full text-left">
-					<tbody>
-						<th>Name</th>
-						<th>Calories</th>
-						<th>Carbs</th>
-						<th>Fats</th>
-						<th>Protein</th>
-						<th>Action</th>
-					</tbody>
-					<tbody className="text-sm">
-						{foodEntries?.map((entry) => (
-							<FoodEntryCard
-								id={entry.id}
-								name={entry.name}
-								calories={entry.calories}
-								carbs={entry.carbs}
-								fats={entry.fats}
-								protein={entry.protein}
-								key={entry.id}
-							/>
-						))}
-					</tbody>
-					<tbody className="border-t border-slate-900 text-sm">
-						<tr>
-							<th>Total</th>
-							<th>{totalCalories}</th>
-							<th>{totalCarbs}g</th>
-							<th>{totalFats}g</th>
-							<th>{totalProtein}g</th>
-						</tr>
-					</tbody>
-				</table>
-			</section>
+			<section className="flex flex-col gap-4 md:flex-row md:justify-around md:gap-0">
+				<section className="flex flex-col items-center gap-4 my-5">
+					<LineDivider />
+					<h2 className="text-2xl font-semibold text-indigo-700">
+						Exercise Journal
+					</h2>
+					{exerciseEntries?.length === 0 && (
+						<p className="font-semibold text-sm text-slate-800">
+							Currently has no entries.
+						</p>
+					)}
+					{exerciseEntries?.length! > 0 && (
+						<table className="w-full text-left">
+							<tbody>
+								<th>Exercise</th>
+								<th>Type</th>
+								<th>Reps</th>
+								<th>Weight</th>
+								<th>Calories</th>
+								<th>Action</th>
+							</tbody>
+							<tbody className="text-sm">
+								{exerciseEntries?.map((entry) => (
+									<ExerciseEntryCard
+										id={entry.id}
+										name={entry.name}
+										type={entry.type}
+										duration={entry.duration}
+										weight={entry.weight}
+										sets={entry.sets}
+										reps={entry.reps}
+										calories={entry.calories}
+										key={entry.id}
+									/>
+								))}
+							</tbody>
+						</table>
+					)}
+				</section>
+				<section className="flex flex-col items-center gap-4 mt-5">
+					<LineDivider />
+					<h2 className="text-2xl font-semibold text-indigo-700">
+						Nutrition Journal
+					</h2>
+					{foodEntries?.length === 0 && (
+						<p className="font-semibold text-sm text-slate-800">
+							Currently has no entries.
+						</p>
+					)}
+					{foodEntries?.length! > 0 && (
+						<table className="w-full text-left">
+							<tbody>
+								<th>Name</th>
+								<th>Calories</th>
+								<th>Carbs</th>
+								<th>Fats</th>
+								<th>Protein</th>
+								<th>Action</th>
+							</tbody>
 
-			<section className="flex flex-col items-center gap-4 mt-5">
-				<LineDivider />
-				<h2 className="text-2xl font-semibold text-indigo-700">
-					Exercise Journal
-				</h2>
-				<table className="w-full text-left">
-					<tbody>
-						<th>Exercise</th>
-						<th>Type</th>
-						<th>Reps</th>
-						<th>Weight</th>
-						<th>Calories</th>
-						<th>Action</th>
-					</tbody>
-					<tbody className="text-sm">
-						{exerciseEntries?.map((entry) => (
-							<ExerciseEntryCard
-								id={entry.id}
-								name={entry.name}
-								type={entry.type}
-								duration={entry.duration}
-								weight={entry.weight}
-								sets={entry.sets}
-								reps={entry.reps}
-								calories={entry.calories}
-								key={entry.id}
-							/>
-						))}
-					</tbody>
-				</table>
+							<tbody className="text-sm">
+								{foodEntries?.map((entry) => (
+									<FoodEntryCard
+										id={entry.id}
+										name={entry.name}
+										calories={entry.calories}
+										carbs={entry.carbs}
+										fats={entry.fats}
+										protein={entry.protein}
+										key={entry.id}
+									/>
+								))}
+							</tbody>
+							<tbody className="border-t border-slate-900 text-sm">
+								<tr>
+									<th>Total</th>
+									<th>{totalCalories}</th>
+									<th>{totalCarbs}g</th>
+									<th>{totalFats}g</th>
+									<th>{totalProtein}g</th>
+								</tr>
+							</tbody>
+						</table>
+					)}
+				</section>
 			</section>
 		</main>
 	);
