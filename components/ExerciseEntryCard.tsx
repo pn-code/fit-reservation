db5bc83b744a4cc55a25582b77d06cbb4c58b0fd
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-21 13:34:02 -0700

    Added delete exercise entry handler

--- components/ExerciseEntryCard.tsx
+++ components/ExerciseEntryCard.tsx
@@ -1,4 +1,8 @@
+"use client"
+import axios from "axios";
 import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 interface ExerciseEntryCardProps {
 	id: number;
@@ -21,6 +25,25 @@ function ExerciseEntryCard({
 	reps,
 	calories,
 }: ExerciseEntryCardProps) {
+	const router = useRouter();
+
+	const handleDeleteExerciseEntry = async (id: number) => {
+		try {
+			await axios.delete("/api/exercise_entries", {
+				data: {
+					id: id,
+				},
+			});
+
+			toast.success(`Successfully deleted item ${name.length > 30 ? name.substring(0, 30) + "..." : name}!`);
+			router.refresh();
+		} catch (error) {
+			toast.error(
+				`An error has occurred while attempting to delete ${name}.`
+			);
+		}
+	};
+
 	return (
 		<>
 			<tr className="hover:bg-slate-200 cursor-pointer hover:text-indigo-600">
@@ -36,7 +59,7 @@ function ExerciseEntryCard({
 				<td>{weight} lbs</td>
 				<td>{calories}</td>
 				<td>
-					<button>
+					<button onClick={() => handleDeleteExerciseEntry(id)}>
 						<X
 							className="hover:bg-slate-300 ml-3 rounded-full"
 							color="red"
