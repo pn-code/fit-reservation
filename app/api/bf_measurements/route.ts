d8bb32aea19a95c8709adc1d62468332ac12c74e
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-21 15:46:13 -0700

    Built POST for BF

---
+++ app/api/bf_measurements/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../lib/client";
+import { currentUser } from "@clerk/nextjs/app-beta";
+
+export async function POST(req: Request) {
+	try {
+		const user = await currentUser();
+
+		if (user) {
+			const res = await req.json();
+
+			const newBodyFatMeasurement =
+				await prisma.bodyFatMeasurement.create({
+					data: {
+						bodyfat: res.bodyfat,
+						userId: user.id,
+					},
+				});
+
+			return NextResponse.json(newBodyFatMeasurement);
+		}
+	} catch (error) {
+		return console.error(error);
+	}
+}
