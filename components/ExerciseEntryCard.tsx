addf6dca863f22c5ef0a0191e7daa49a0ad03492
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-21 11:10:11 -0700

    Initial build of exercise entry

---
+++ components/ExerciseEntryCard.tsx
@@ -0,0 +1,27 @@
+import { X } from "lucide-react";
+import React from "react";
+
+function ExerciseEntryCard() {
+	return (
+		<>
+			<tr className="hover:bg-slate-200 cursor-pointer hover:text-indigo-600">
+				<td>
+                    exercise
+					{/* {name.length > 30 ? name.substring(0, 30) + "..." : name} */}
+				</td>
+				<td>type</td>
+				<td>3 x 10</td>
+				<td>
+					<button>
+						<X
+							className="hover:bg-slate-300 ml-3 rounded-full"
+							color="red"
+						/>
+					</button>
+				</td>
+			</tr>
+		</>
+	);
+}
+
+export default ExerciseEntryCard;
