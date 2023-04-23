be1651998c6abef64bb8ce4533bfc8b80548194a
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-23 15:06:42 -0700

    Updating links of navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,18 +5,20 @@ const Navbar = () => {
 	return (
 		<nav className="bg-blue-900 text-white py-6 px-6 flex justify-between items-center fixed w-full top-0 left-0 z-50">
 			<header className="flex items-center gap-4">
-				<Image
-					className="rounded-sm cursor-pointer"
-					src="/assets/logo.png"
-					alt="FitHeroes Logo"
-					width={160}
-					height={100}
-				/>
+				<Link className="hover:underline font-medium" href="/dashboard">
+					<Image
+						className="rounded-sm cursor-pointer"
+						src="/assets/logo.png"
+						alt="FitHeroes Logo"
+						width={160}
+						height={100}
+					/>
+				</Link>
 			</header>
 
 			<ul className="flex gap-4">
-				<Link className="hover:underline font-medium" href="/">
-					Home
+				<Link className="hover:underline font-medium" href="/dashboard">
+					Dashboard
 				</Link>
 				<Link
 					className="hover:underline font-medium"
