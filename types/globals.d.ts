547fbfef8783afc7bbbeb011019b60f9befea413
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-24 13:35:56 -0700

    Built WeightMeasurement & BodyFatMeasurement types

---
+++ types/globals.d.ts
@@ -0,0 +1,13 @@
+interface WeightMeasurement {
+	id: number;
+	weight: number;
+	createdAt: Date;
+	userId: string;
+}
+
+interface BodyFatMeasurement {
+	id: number;
+	bodyfat: number;
+	createdAt: Date;
+	userId: string;
+}
