534cfb4644c3351c41e60e0b35f2ac8f3edbe665
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-24 16:35:51 -0700

    Another attempt at fixing type error

--- app/api/bf_measurements/route.ts
+++ app/api/bf_measurements/route.ts
@@ -35,7 +35,7 @@ export async function POST(req: Request) {
 			const newBodyFatMeasurement =
 				await prisma.bodyFatMeasurement.create({
 					data: {
-						bodyfat: res.bodyfat,
+						bodyfat: Number(res.bodyfat),
 						userId: user.id,
 					} as BodyFatMeasurementInput,
 				});
