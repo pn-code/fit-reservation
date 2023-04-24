1a7c05750975cb25b37e36b5620762a14e16503c
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-24 12:50:08 -0700

    Added input handlers @ measurement forms

--- components/MeasurementsForm.tsx
+++ components/MeasurementsForm.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-function MeasurementsForm() {
-	return (
-		<section className="flex flex-col gap-4 md:flex-row md:justify-start">
-			<form className="flex flex-col gap-4 md:flex-row md:justify-start md:items-center">
-				<label htmlFor="weight">Current Weight (lbs):</label>
-				<input type="number" id="weight" />
-				<button className="bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline">
-					Submit Weight
-				</button>
-			</form>
+import { useState } from "react";
 
-			<form className="flex flex-col gap-2 md:flex-row md:justify-start md:items-center">
-				<label htmlFor="bf">Current Body Fat (%):</label>
-				<input type="number" id="bf" />
-				<button className="bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline">
-					Submit Body Fat
-				</button>
-			</form>
-		</section>
+function MeasurementsForm() {
+	const [weight, setWeight] = useState<number>()
+	const [bodyFat, setBodyFat] = useState<number>()
+	
+	return (
+		<>
+			<h2 className="text-slate-800 text-2xl font-semibold">
+				Add Measurements
+			</h2>
+			
+			<section className="flex flex-col gap-4 md:flex-row md:justify-start">
+				<form className="flex flex-col gap-4 md:flex-row md:justify-start md:items-center">
+					<label htmlFor="weight">Current Weight (lbs):</label>
+					<input onChange={(e) => setWeight(Number(e.target.value))} value={weight} type="number" id="weight" placeholder="weight"/>
+					<button className="bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline">
+						Submit Weight
+					</button>
+				</form>
+
+				<form className="flex flex-col gap-2 md:flex-row md:justify-start md:items-center">
+					<label htmlFor="bf">Current Body Fat (%):</label>
+					<input onChange={(e) => setBodyFat(Number(e.target.value))} value={bodyFat}  type="number" id="bf" placeholder="Body Fat Percentage"/>
+					<button className="bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline">
+						Submit Body Fat
+					</button>
+				</form>
+			</section>
+		</>
 	);
 }
 
