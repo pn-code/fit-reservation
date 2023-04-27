5072ebbe5399df8bded5d73792e785c187aa04e6
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-26 20:16:02 -0700

    Updated Navbar colors & removed fixed option

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,14 @@
+import { Shield } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
 	return (
-		<nav className="bg-blue-900 text-white py-6 px-6 flex justify-between items-center fixed w-full top-0 left-0 z-50">
-			<header className="flex items-center gap-4">
-				<Link className="hover:underline font-medium" href="/dashboard">
-					<Image
-						className="rounded-sm cursor-pointer"
-						src="/assets/logo.png"
-						alt="FitHeroes Logo"
-						width={160}
-						height={100}
-					/>
+		<nav className="h-[100px] bg-[#172554] text-white py-6 px-6 flex justify-between items-center w-full top-0 left-0 z-50">
+			<header>
+				<Link className="hover:underline font-medium flex gap-2 items-center" href="/dashboard">
+					<Shield size={40}/>
+					<h1 className="text-3xl font-bold ">FitHeroes</h1>
 				</Link>
 			</header>
 
