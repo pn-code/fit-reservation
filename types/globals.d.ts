1a8c96e47d13c8904687ac2c32d052a47978d067
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-27 22:29:45 -0700

    Added ExerciseEntry interface

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,13 +1,26 @@
 interface WeightMeasurement {
-	id: number;
-	weight: number;
-	createdAt: Date;
-	userId: string;
+    id: number;
+    weight: number;
+    createdAt: Date;
+    userId: string;
 }
 
 interface BodyFatMeasurement {
-	id: number;
-	bodyfat: number;
-	createdAt: Date;
-	userId: string;
+    id: number;
+    bodyfat: number;
+    createdAt: Date;
+    userId: string;
+}
+
+interface ExerciseEntry {
+    calories: number;
+    date: string;
+    duration: number;
+    id: number;
+    name: string;
+    reps: number;
+    sets: number;
+    type: string;
+    userId: string;
+    weight: number;
 }
