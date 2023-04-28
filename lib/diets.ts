4d9e8f124e122440a4fdbe2dba462ae705555d3f
Philip Nguyen <philipnguyen953@gmail.com>
2023-04-28 12:08:06 -0700

    Built diets

---
+++ lib/diets.ts
@@ -0,0 +1,7 @@
+export const diets: DietMacros = {
+    zone: { carbs: 40, fats: 30, protein: 30 },
+    paleo: { carbs: 25, fats: 40, protein: 35 },
+    med: { carbs: 50, fats: 30, protein: 20 },
+    keto: { carbs: 5, fats: 70, protein: 25 },
+    lowfat: { carbs: 65, fats: 15, protein: 20 },
+};
