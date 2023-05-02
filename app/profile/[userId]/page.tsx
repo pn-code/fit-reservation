78b4cf36638918953086a18a79cfd5afc8577284
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-02 12:12:33 -0700

    Made btn it's own component & using params

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,30 +1,22 @@
-"use client";
 import Image from "next/image";
-import { useAuth } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { useUser } from "@clerk/nextjs";
+import SignOutButton from "../../../components/SignOutButton";
+import { clerkClient } from "@clerk/nextjs/app-beta";
 
-function ProfilePage() {
-    const { signOut } = useAuth();
-    const router = useRouter();
-
-    const user = useUser().user;
-
-    const signOutUser = async () => {
-        await signOut();
-        router.refresh();
+interface Props {
+    params: {
+        userId: string;
     };
+}
 
+async function ProfilePage({ params }: Props) {
+    const { userId } = params;
+    const user = await clerkClient.users.getUser(userId);
+    
     return (
         <main className="w-full h-full bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-10 text-white/90">
             <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
                 <h1 className="text-3xl">Profile</h1>
-                <button
-                    className="px-4 py-2 bg-red-600 rounded-md text-white hover:bg-red-700 font-semibold"
-                    onClick={signOutUser}
-                >
-                    Sign out
-                </button>
+                <SignOutButton />
             </header>
 
             {/* User Information */}
@@ -36,7 +28,9 @@ function ProfilePage() {
                     width={60}
                     height={60}
                 />
-                <h2 className="text-2xl font-semibold text-amber-300">{user?.fullName}</h2>
+                <h2 className="text-2xl font-semibold text-amber-300">
+                    {user?.fullName}
+                </h2>
             </section>
         </main>
     );
