325d4624991b884212a81fcc3b20a835ec8c545b
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-02 12:56:23 -0700

    Added responsive design to tracker header

--- components/TrackerHeader.tsx
+++ components/TrackerHeader.tsx
@@ -3,46 +3,44 @@ import React from "react";
 import Link from "next/link";
 
 interface TrackerHeaderProps {
-	title: String;
+    title: String;
 }
 
 function TrackerHeader({ title }: TrackerHeaderProps) {
-	return (
-		<header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
-			<h1 className="text-3xl">{title}</h1>
-			<nav>
-				<ul className="flex gap-5 text-white">
-					<li
-						className={
-							title == "Tracker Overview"
-								? "hidden"
-								: "bg-slate-600 hover:bg-slate-700 rounded-lg text-white px-4 py-2 hover:underline"
-						}
-					>
-						<Link href="/tracker/overview">Overview</Link>
-					</li>
-					<li
-						className={
-							title == "Exercise"
-								? "hidden"
-								: "bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline"
-						}
-					>
-						<Link href="/tracker/exercise">Exercise</Link>
-					</li>
-					<li
-						className={
-							title == "Nutrition"
-								? "hidden"
-								: "bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2 hover:underline"
-						}
-					>
-						<Link href="/tracker/nutrition">Nutrition</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+    return (
+        <header className="flex flex-col gap-4 sm:flex-row sm:justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
+            <h1 className="text-3xl">{title}</h1>
+            <nav>
+                <ul className="w-full flex gap-5 text-white mb-2">
+                    <li className={title == "Tracker Overview" ? "hidden" : ""}>
+                        <Link
+                            className="bg-slate-600 hover:bg-slate-700 rounded-lg text-white px-2 sm:px-4 py-2"
+                            href="/tracker/overview"
+                        >
+                            Overview
+                        </Link>
+                    </li>
+                    <li className={title == "Exercise" ? "hidden" : ""}>
+                        <Link
+                            className=" bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-2 sm:px-4 py-2"
+                            href="/tracker/exercise"
+                        >
+                            Exercise
+                        </Link>
+                    </li>
+                    <li
+                        className={
+                            title == "Nutrition"
+                                ? "hidden"
+                                : "bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-2"
+                        }
+                    >
+                        <Link href="/tracker/nutrition">Nutrition</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default TrackerHeader;
