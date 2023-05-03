8f35147811cd18b1538adbfb1201104db4a915b0
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-03 14:06:04 -0700

    Built out upgrade profile section

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -11,7 +11,7 @@ interface Props {
 async function ProfilePage({ params }: Props) {
     const { userId } = params;
     const user = await clerkClient.users.getUser(userId);
-    
+
     return (
         <main className="w-full h-full bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-10 text-white/90">
             <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
@@ -29,8 +29,23 @@ async function ProfilePage({ params }: Props) {
                     height={60}
                 />
                 <h2 className="text-2xl font-semibold text-amber-300">
-                {`${user?.firstName} ${user?.lastName}`}
+                    {`${user?.firstName} ${user?.lastName}`}
                 </h2>
+            </section>
+
+            {/* User Stats */}
+
+            {/* Upgrade Profile */}
+            <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+                <section>
+                    <h2 className="text-2xl font-semibold text-amber-300">
+                        Upgrade account to premium
+                    </h2>
+                    <p>Gain immediate access to the Builder Page</p>
+                </section>
+                <button className="rounded-md px-4 py-2 bg-green-500 hover:bg-green-600 text-white/90 hover:text-white font-semibold">
+                    Upgrade
+                </button>
             </section>
         </main>
     );
