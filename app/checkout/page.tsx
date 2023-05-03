4c7b9b927750de1bb4d6a33df7ac48685f67512c
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-03 16:02:32 -0700

    Built checkout page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+export default function PreviewPage() {
+    const router = useRouter();
+    useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+        if (query.get("success")) {
+            console.log(
+                "Order placed! You will receive an email confirmation."
+            );
+        }
+
+        if (query.get("canceled")) {
+            console.log(
+                "Order canceled -- continue to shop around and checkout when you’re ready."
+            );
+        }
+    }, []);
+
+    const handleCheckOut = async () => {
+        try {
+            const res = await axios.post("/api/checkout_sessions");
+            console.log(res.data);
+            router.push(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <form>
+            <section>
+                <button onClick={handleCheckOut} type="button" role="link">
+                    Checkout
+                </button>
+            </section>
+        </form>
+    );
+}
