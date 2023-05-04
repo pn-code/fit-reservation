569622d525e0cff7f56d0cd3859bafa233b3ff93
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-03 20:05:42 -0700

    Conditional rendering when user is subscribed

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import SignOutButton from "../../../components/SignOutButton";
 import { clerkClient } from "@clerk/nextjs/app-beta";
 import StripeCheckOutButton from "../../../components/StripeCheckOutButton";
+import { prisma } from "../../../lib/client";
+import { Check } from "lucide-react";
 
 interface Props {
     params: {
@@ -12,6 +14,9 @@ interface Props {
 async function ProfilePage({ params }: Props) {
     const { userId } = params;
     const user = await clerkClient.users.getUser(userId);
+    const userIsSubscribed = await prisma.subscription.findFirst({
+        where: { userId: user.id },
+    });
 
     return (
         <main className="w-full h-full bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-10 text-white/90">
@@ -32,20 +37,27 @@ async function ProfilePage({ params }: Props) {
                 <h2 className="text-2xl font-semibold text-amber-300">
                     {`${user?.firstName} ${user?.lastName}`}
                 </h2>
+                {userIsSubscribed && (
+                    <span className="bg-indigo-600 rounded-full p-1 border-2 border-indigo-700 flex justify-center items-center">
+                        <Check />
+                    </span>
+                )}
             </section>
 
             {/* User Stats */}
 
             {/* Upgrade Profile */}
-            <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
-                <section>
-                    <h2 className="text-2xl font-semibold text-amber-300">
-                        Upgrade account to premium
-                    </h2>
-                    <p>Gain immediate access to the Builder Page</p>
+            {!userIsSubscribed && (
+                <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+                    <section>
+                        <h2 className="text-2xl font-semibold text-amber-300">
+                            Upgrade account to premium
+                        </h2>
+                        <p>Gain immediate access to the Builder Page</p>
+                    </section>
+                    <StripeCheckOutButton />
                 </section>
-                <StripeCheckOutButton />
-            </section>
+            )}
         </main>
     );
 }
