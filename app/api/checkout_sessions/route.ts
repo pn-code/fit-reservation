39fe1cd00868fe553ed8a075ece3f73161f69b7c
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-03 17:29:25 -0700

    Changing urls to dynamic url

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 import Stripe from "stripe";
 
 export async function POST() {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
         apiVersion: "2022-11-15",
     });
+
+    const headersList = headers();
+    const currentDomain = headersList.get("origin");
 
     try {
         const session = await stripe.checkout.sessions.create({
@@ -15,8 +19,8 @@ export async function POST() {
                 },
             ],
             mode: "subscription",
-            success_url: `https://localhost:3000/?success=true`,
-            cancel_url: `https://localhost:3000/?canceled=true`,
+            success_url: `${currentDomain}/?success=true`,
+            cancel_url: `${currentDomain}/?canceled=true`,
         });
         return NextResponse.json(session.url);
     } catch (error) {
