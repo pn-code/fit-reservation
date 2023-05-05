a508c67b16b4e5ad5e821fc8379e8de8154c710b
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-05 14:25:28 -0700

    Added tool section to improve landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Calculator, Dumbbell, FileBarChart } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="w-full h-[calc(100vh-82px)] bg-gradient-to-b from-[#172554] to-blue-900 flex flex-col gap-4 shadow-md text-white/90 px-10">
+        <main className="w-full h-full bg-gradient-to-b from-[#172554] to-blue-900 flex flex-col gap-4 shadow-md text-white/90 px-10 pb-10">
             <header className="mt-[20%] sm:mt-56 sm:mb-14 w-full">
-                <h1 className="text-center text-4xl font-bold tracking-tight sm:text-6xl sm:tracking-tight lg:text-[4rem] xl:text-[6rem] xl:tracking-tight 2xl:text-[6.5rem]">
+                <h1 className="text-center text-5xl font-bold tracking-tight sm:text-5xl sm:tracking-tight lg:text-[4rem] xl:text-[6rem] xl:tracking-tight 2xl:text-[6.5rem]">
                     The best way to <span>jumpstart</span> your
                     <span className="text-indigo-400"> epic</span>
                     <span className="text-blue-600"> fitness</span>
@@ -12,21 +13,75 @@ export default function Home() {
                 </h1>
             </header>
 
-            <section className="w-full flex items-center justify-center gap-5 text-lg font-semibold">
+            <section className="mt-10 sm:mt-10 w-full flex items-center justify-center gap-5 text-lg font-semibold">
                 <Link
                     passHref={true}
                     href="/register"
-                    className="hover:text-white py-2 px-8 bg-slate-900 hover:bg-slate-800 rounded-full"
+                    className="hover:text-white py-2 px-8 bg-slate-900 hover:bg-slate-800 rounded-md"
                 >
-                    Start Now
+                    Start
                 </Link>
                 <Link
                     passHref={true}
-                    className="text-black/90 hover:text-black py-2 px-8 bg-slate-200 hover:bg-slate-50 rounded-full"
+                    className="text-black/90 hover:text-black py-2 px-8 bg-slate-200 hover:bg-slate-50 rounded-md"
                     href={"/login"}
                 >
                     Login
                 </Link>
+            </section>
+
+            {/* FEATURES SECTION */}
+            <section className="flex justify-center w-full mt-20">
+                <section className="flex flex-col gap-8">
+                <h1 className="font-black text-3xl sm:text-5xl sm:mb-10">
+                    USE OUR TOOLS TO:
+                </h1>
+                {/* CALCULATE */}
+                <div className="flex gap-8 sm:gap-16 hover:scale-105 duration-200 ease-linear lg:w-[575px]">
+                    <div className="flex justify-center items-center p-2 bg-indigo-600 w-[70px] h-[70px] sm:h-52 sm:w-52 rounded-xl">
+                        <Calculator size={100}/>
+                    </div>
+                    <div>
+                        <h2 className="text-2xl font-bold sm:text-5xl mb-2">
+                            CALCULATE
+                        </h2>
+                        <span className="text-sm sm:text-2xl">
+                            Find your starting point.
+                        </span>
+                    </div>
+                </div>
+
+                {/* PLAN */}
+                <div className="flex gap-8 sm:gap-16 hover:scale-105 duration-200 ease-linear">
+                    <div className="flex justify-center items-center p-2 bg-indigo-600 w-[70px] h-[70px] sm:h-52 sm:w-52 rounded-xl">
+                        <Dumbbell size={100}/>
+                    </div>
+                    <div>
+                        <h2 className="text-2xl font-bold sm:text-5xl mb-2">
+                            PLAN
+                        </h2>
+                        <span className="text-sm sm:text-2xl">
+                            Take control of your workouts.
+                        </span>
+                    </div>
+                </div>
+
+                {/* TRACK */}
+                <div className="flex gap-8 sm:gap-16 hover:scale-105 duration-200 ease-linear">
+                    <div className="flex justify-center items-center p-2 bg-indigo-600 w-[70px] h-[70px] sm:h-52 sm:w-52 rounded-xl">
+                        <FileBarChart size={100}/>
+                    </div>
+
+                    <div>
+                        <h2 className="text-2xl font-bold sm:text-5xl mb-2">
+                            TRACK
+                        </h2>
+                        <span className="text-sm sm:text-2xl">
+                            Add success to your to do list.
+                        </span>
+                    </div>
+                </div>
+                </section>
             </section>
         </main>
     );
