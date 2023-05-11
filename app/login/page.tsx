7873fcf841dfca7d0b3abf126ccaec1a52bfe275
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-11 16:45:20 -0700

    Centered login/register pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,9 +6,9 @@ export const metadata = {
 };
 
 export default function LoginPage() {
-	return (
-		<main className="w-full h-full bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90">
-			<SignIn signUpUrl="/register" />
-		</main>
-	);
+    return (
+        <main className="w-full h-[calc(100vh-90px)] bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90 justify-center items-center">
+            <SignIn signUpUrl="/register" />
+        </main>
+    );
 }
