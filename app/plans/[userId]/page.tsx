654e027cd60300a87247f086d97f79eb40ced177
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-22 19:03:01 -0700

    Add explore link & mobile styles

--- app/plans/[userId]/page.tsx
+++ app/plans/[userId]/page.tsx
@@ -1,4 +1,5 @@
 import { clerkClient } from "@clerk/nextjs/server";
+import { GlobeIcon, HammerIcon } from "lucide-react";
 import Link from "next/link";
 
 export const metadata = {
@@ -16,12 +17,22 @@ export default async function Plans({ params }: Props) {
         <main className="w-full h-full bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90">
             <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
                 <h1 className="text-3xl">{`${user.firstName} ${user.lastName}'s  Plans`}</h1>
-                <Link
-                    className="bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white px-4 py-3"
-                    href="/plans/explore"
-                >
-                    Explore Plans
-                </Link>
+                <section className="flex gap-4">
+                    <Link
+                        className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-700 rounded-lg text-white p-2"
+                        href="/plans/explore"
+                    >
+                        <HammerIcon size={24}/>
+                        <span className="hidden sm:flex">Build</span>
+                    </Link>
+                    <Link
+                        className="flex justify-center items-center gap-2 bg-indigo-600 hover:bg-indigo-700 rounded-lg text-white p-2"
+                        href="/plans/explore"
+                    >
+                        <GlobeIcon size={24}/>
+                        <span className="hidden sm:flex">Explore</span>
+                    </Link>
+                </section>
             </header>
 
             {/* User Plans Here */}
