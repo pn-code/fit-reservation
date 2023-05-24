0cb2e8447bef25d3f2a54014a6db4cce3a1c6c35
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-24 12:43:26 -0700

    Created POST endpoint for Training Plan

---
+++ app/api/plans/route.tsx
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../lib/client";
+import { currentUser } from "@clerk/nextjs/app-beta";
+
+export async function POST(req: Request) {
+    const data = await req.json();
+    const user = await currentUser();
+
+    try {
+        if (user && data) {
+            const trainingPlan = await prisma.trainingPlan.create({
+                data: {
+                    userId: user.id,
+                    name: data.name,
+                    description: data.description,
+                    exercises: {
+                        create: data.exercises,
+                    },
+                },
+                include: {
+                    exercises: true,
+                },
+            });
+
+            return NextResponse.json(trainingPlan);
+        } else {
+            return NextResponse.error()
+        }
+    } catch (error) {
+        return NextResponse.error()
+    }
+}
