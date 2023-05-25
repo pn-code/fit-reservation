808409394d3ae0f4806989146fd3bdf14b7b7f07
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-24 21:56:27 -0700

    Updated training plan card w/ new info

--- components/TrainingPlanCard.tsx
+++ components/TrainingPlanCard.tsx
@@ -1,18 +1,29 @@
-import React from "react";
-import { getUserNameFromUserId } from "../helpers/getUserNameFromUserId";
+import Link from "next/link";
+import RatingComponent from "./RatingComponent";
 
 interface Props {
     plan: TrainingPlan;
 }
 
-export default async function TrainingPlanCard({ plan }: Props) {
-    const authorFullName = await getUserNameFromUserId(plan.userId);
-
+export default function TrainingPlanCard({ plan }: Props) {
     return (
-        <article>
-            <header>
-                <h3>{plan.name}</h3> by <span>{authorFullName}</span>
-            </header>
-        </article>
+        <Link href={`/plans/details/${plan.id}`}>
+            <article className="flex flex-col gap-4 bg-blue-900/70 w-full py-4 px-1 rounded-md hover:bg-blue-900 cursor-pointer">
+                <header className="flex justify-between items-center">
+                    <h3 className="inline">{plan.name}</h3>
+
+                    <section>
+                        <RatingComponent reviews={plan.reviews} />
+                    </section>
+                </header>
+
+                <section>
+                    <h3>Description:</h3>
+                    <p className="text-sm">
+                        {plan.description.substring(0, 30)}...
+                    </p>
+                </section>
+            </article>
+        </Link>
     );
 }
