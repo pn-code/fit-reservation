b6b81d079988621898100d6198269efd682b53ae
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-25 19:01:06 -0700

    Updated styles and organization of divs

--- components/BuildPlanForm.tsx
+++ components/BuildPlanForm.tsx
@@ -31,120 +31,127 @@ export default function BuildPlanForm() {
     const handleSubmitPlan = async () => {
         const planObj = { name: planName, description, exercises };
         const res = await axios.post("/api/plans", planObj);
-
-
     };
 
     return (
-        <section className="flex flex-col gap-4 lg:flex-row">
-            <form className="flex flex-col gap-4 justify-center bg-gray-800 px-4 py-2 rounded-md">
-                <h2 className="text-lg font-bold">Plan Details</h2>
-                {/* Plan Name */}
-                <section className="flex flex-col gap-2">
-                    <label htmlFor="name">Plan Name</label>
-                    <input
-                        id="name"
-                        className="w-full sm:w-72"
-                        type="text"
-                        value={planName}
-                        placeholder="Add Plan Name"
-                        onChange={(e) => setPlanName(e.target.value)}
-                    />
-                </section>
-
-                {/* Description */}
-                <section className="flex flex-col gap-2">
-                    <label htmlFor="description">Plan Description</label>
-                    <textarea
-                        id="description"
-                        className="w-full sm:w-72 p-2 rounded-sm text-black"
-                        value={description}
-                        placeholder="Add Description"
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </section>
-
-                <form className="flex flex-col gap-4 justify-center bg-gray-800 py-2 rounded-md">
-                    <h2 className="text-lg font-bold">Add Exercises</h2>
-                    {/* Exercise Name */}
-                    <section className="flex flex-col gap-2">
-                        <label htmlFor="exercise">Exercise</label>
-                        <input
-                            id="exercise"
-                            className="w-full sm:w-72"
-                            type="text"
-                            value={exercise}
-                            placeholder="Add exercise"
-                            onChange={(e) => setExercise(e.target.value)}
-                        />
+        <section className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4 justify-center bg-gray-800 px-4 py-2 rounded-md sm:flex-row">
+                {/* Form Section */}
+                <section className="flex flex-col w-full gap-4">
+                    <section className="w-full flex flex-col gap-4">
+                        <h2 className="text-lg font-bold">Plan Details</h2>
+                        {/* Plan Name */}
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="name">Plan Name</label>
+                            <input
+                                id="name"
+                                className="w-full sm:w-72"
+                                type="text"
+                                value={planName}
+                                placeholder="Add Plan Name"
+                                onChange={(e) => setPlanName(e.target.value)}
+                            />
+                        </section>
+                        {/* Description */}
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="description">
+                                Plan Description
+                            </label>
+                            <textarea
+                                id="description"
+                                className="w-full sm:w-72 p-2 rounded-sm text-black"
+                                value={description}
+                                placeholder="Add Description"
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </section>
                     </section>
 
-                    {/* Type */}
-                    <section className="flex flex-col gap-2">
-                        <label htmlFor="type">Type</label>
-                        <select
-                            className="w-full sm:w-72"
-                            name="type"
-                            id="type"
-                            onChange={(e) => setType(e.target.value)}
-                            value={type}
+                    <section className="w-full flex flex-col gap-2">
+                        <h2 className="text-lg font-bold">Add Exercises</h2>
+                        {/* Exercise Name */}
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="exercise">Exercise</label>
+                            <input
+                                id="exercise"
+                                className="w-full sm:w-72"
+                                type="text"
+                                value={exercise}
+                                placeholder="Add exercise"
+                                onChange={(e) => setExercise(e.target.value)}
+                            />
+                        </section>
+
+                        {/* Type */}
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="type">Type</label>
+                            <select
+                                className="w-full sm:w-72"
+                                name="type"
+                                id="type"
+                                onChange={(e) => setType(e.target.value)}
+                                value={type}
+                            >
+                                <option value="resistance">Resistance</option>
+                                <option value="cardio">Cardio</option>
+                            </select>
+                        </section>
+
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="sets">Sets</label>
+                            <input
+                                id="sets"
+                                className="w-full sm:w-72"
+                                type="text"
+                                value={sets}
+                                placeholder="Add sets"
+                                onChange={(e) =>
+                                    setSets(Number(e.target.value))
+                                }
+                            />
+                        </section>
+
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="reps">Reps</label>
+                            <input
+                                id="reps"
+                                className="w-full sm:w-72"
+                                type="text"
+                                value={reps}
+                                placeholder="Add reps"
+                                onChange={(e) =>
+                                    setReps(Number(e.target.value))
+                                }
+                            />
+                        </section>
+
+                        <section className="flex flex-col gap-2">
+                            <label htmlFor="duration">Duration (min)</label>
+                            <input
+                                id="duration"
+                                className="w-full sm:w-72"
+                                type="text"
+                                value={duration}
+                                placeholder="Add duration in minutes"
+                                onChange={(e) =>
+                                    setDuration(Number(e.target.value))
+                                }
+                            />
+                        </section>
+
+                        <button
+                            onClick={addExerciseToList}
+                            type="button"
+                            className="w-full sm:w-72 bg-green-600 hover:bg-green-700 rounded-lg mt-4 text-white px-4 py-2 hover:underline"
                         >
-                            <option value="resistance">Resistance</option>
-                            <option value="cardio">Cardio</option>
-                        </select>
+                            Add Exercise
+                        </button>
                     </section>
+                </section>
 
-                    <section className="flex flex-col gap-2">
-                        <label htmlFor="sets">Sets</label>
-                        <input
-                            id="sets"
-                            className="w-full sm:w-72"
-                            type="text"
-                            value={sets}
-                            placeholder="Add sets"
-                            onChange={(e) => setSets(Number(e.target.value))}
-                        />
-                    </section>
-
-                    <section className="flex flex-col gap-2">
-                        <label htmlFor="reps">Reps</label>
-                        <input
-                            id="reps"
-                            className="w-full sm:w-72"
-                            type="text"
-                            value={reps}
-                            placeholder="Add reps"
-                            onChange={(e) => setReps(Number(e.target.value))}
-                        />
-                    </section>
-
-                    <section className="flex flex-col gap-2">
-                        <label htmlFor="duration">Duration (min)</label>
-                        <input
-                            id="duration"
-                            className="w-full sm:w-72"
-                            type="text"
-                            value={duration}
-                            placeholder="Add duration in minutes"
-                            onChange={(e) =>
-                                setDuration(Number(e.target.value))
-                            }
-                        />
-                    </section>
-
-                    <button
-                        onClick={addExerciseToList}
-                        type="button"
-                        className="w-full sm:w-72 bg-green-600 hover:bg-green-700 rounded-lg mt-4 text-white px-4 py-2 hover:underline"
-                    >
-                        Add Exercise
-                    </button>
-                </form>
-
-                {/* Exercise List */}
-                <section className="w-full">
+                <section className="w-full flex flex-col gap-4">
                     <h2 className="text-lg font-bold">Exercise List</h2>
-
+                    {/* Exercise List */}
                     <table className="table-auto w-full">
                         <thead className="h-8 text-xs sm:text-[16px] font-semibold uppercase text-yellow-50 bg-blue-900/60">
                             <tr className="p-2 whitespace-nowrap">
@@ -191,15 +198,16 @@ export default function BuildPlanForm() {
                             ))}
                         </tbody>
                     </table>
+                    <section className="flex justify-end">
+                        <button
+                            onClick={handleSubmitPlan}
+                            type="button"
+                            className="w-full sm:w-40 bg-indigo-600 hover:bg-indigo-700 rounded-lg mt-4 text-white px-4 py-2 hover:underline"
+                        >
+                            Submit
+                        </button>
+                    </section>
                 </section>
-
-                <button
-                    onClick={handleSubmitPlan}
-                    type="button"
-                    className="w-full sm:w-72 bg-indigo-600 hover:bg-indigo-700 rounded-lg mt-4 text-white px-4 py-2 hover:underline"
-                >
-                    Submit
-                </button>
             </form>
         </section>
     );
