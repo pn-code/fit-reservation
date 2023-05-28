c0949c0babdcb7a74804bacd4205b65d1fc1477f
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-27 17:32:26 -0700

    Created getSavedPlans to allow users to save plans

--- app/plans/[userId]/page.tsx
+++ app/plans/[userId]/page.tsx
@@ -28,12 +28,30 @@ export default async function Plans({ params }: Props) {
         return userPlans;
     }
 
+    async function getSavedPlans(): Promise<any> {
+        const savedPlans = await prisma.trainingPlan.findMany({
+            where: {
+                NOT: { userId: user.id },
+                savedByUsers: {
+                    some: {
+                        userId: user.id,
+                    },
+                },
+            },
+            include: { reviews: true },
+        });
+        return savedPlans;
+    }
+
     const userPlans = await getUserPlans();
+    const savedPlans = await getSavedPlans();
+
+    console.log(savedPlans);
 
     return (
-        <main className="w-full h-[calc(100vh-90px)] bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90">
+        <main className="w-full min-h-[calc(100vh-90px)] bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90">
             <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
-                <h1 className="text-3xl">{`${user.firstName} ${user.lastName}'s Plans`}</h1>
+                <h1 className="text-3xl font-bold">{`${user.firstName} ${user.lastName}'s Plans`}</h1>
                 <section className="flex gap-4">
                     <Link
                         className="flex justify-center items-center gap-2 bg-green-600 hover:bg-green-700 rounded-lg text-white p-2"
@@ -52,12 +70,28 @@ export default async function Plans({ params }: Props) {
                 </section>
             </header>
 
-            {/* User Plans Here */}
+            {/* User Plans */}
             <section className="flex flex-col gap-2">
                 {userPlans.map((plan: any) => (
                     <TrainingPlanCard plan={plan} key={plan.id} />
                 ))}
                 {userPlans.length === 0 && (
+                    <p className="text-sm">
+                        Nothing to see here. Create your first plan to see it
+                        here.
+                    </p>
+                )}
+            </section>
+
+            {/* Saved Plans */}
+            <section className="flex flex-col gap-2">
+                <h1 className="text-3xl font-bold pb-2 border-b-2 border-b-indigo-600">
+                    Saved Plans
+                </h1>
+                {savedPlans.map((plan: any) => (
+                    <TrainingPlanCard plan={plan} key={plan.id} />
+                ))}
+                {savedPlans.length === 0 && (
                     <p className="text-sm">
                         Nothing to see here. Create your first plan to see it
                         here.
