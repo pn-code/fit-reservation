39cac9a22b280396cb5a7b5cf8574c2a1a4366d4
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-30 13:46:19 -0700

    Updated localTimeGetter function

--- helpers/getLocalTimezone.ts
+++ helpers/getLocalTimezone.ts
@@ -1,17 +1,31 @@
 const getLocalTimezones = () => {
-	const today = new Date();
-	const timeZoneOffset = today.getTimezoneOffset() * 60000;
-	const localOffset = new Date().getTimezoneOffset() * 60000;
+    const today = new Date();
 
-	const startOfDay = new Date(
-		today.getFullYear(),
-		today.getMonth(),
-		today.getDate()
-	);
-	const endOfDay = new Date(today.getTime() - timeZoneOffset + localOffset);
-	endOfDay.setHours(23, 59, 59, 999);
+    const localStartOfDay = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate(),
+        0, // Set hours to 0 for start of day
+        0, // Set minutes to 0
+        0, // Set seconds to 0
+        0 // Set milliseconds to 0
+    );
 
-	return { startOfDay, endOfDay };
+    const localEndOfDay = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate(),
+        23, // Set hours to 23 for end of day
+        59, // Set minutes to 59
+        59, // Set seconds to 59
+        999 // Set milliseconds to 999
+    );
+
+    const startOfDay = new Date(localStartOfDay.getTime());
+
+    const endOfDay = new Date(localEndOfDay.getTime());
+
+    return { startOfDay, endOfDay };
 };
 
 export default getLocalTimezones;
