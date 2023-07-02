b5952f842987fae0d9daa09976cf6cd03a57cbd6
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-02 15:14:22 -0700

    Moved Guest credentials above login component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,14 +7,16 @@ export const metadata = {
 
 export default function LoginPage() {
     return (
-        <main className="w-full min-h-[calc(100vh-90px)] bg-slate-800 py-6 rounded-md flex flex-col sm:flex-row gap-4 shadow-md px-2 sm:px-10 text-white/90 justify-center items-center">
-            <SignIn signUpUrl="/register" />
-            <section className="flex flex-col gap-1 bg-white rounded-md p-6 text-black">
+        <main className="w-full min-h-[calc(100vh-90px)] bg-slate-800 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90 justify-center items-center">
+            <section className="w-[395px] flex flex-col gap-1 bg-white rounded-md p-6 text-black">
                 <h1 className="text-lg font-bold">Guest Account Credentials</h1>
-                <p className="text-indigo-600 font-[600]">Try out our services.</p>
+                <p className="text-indigo-600 font-[600]">
+                    Try out our services.
+                </p>
                 <span>Email: guest@fitheroes.com</span>
                 <span>Password: 9b7bc049dbd!</span>
             </section>
+            <SignIn signUpUrl="/register" />
         </main>
     );
 }
