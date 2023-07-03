c09aeb3d645a4cc564d6084cab6c66d43aea376a
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-03 12:07:11 -0700

    Added input for date

--- components/BodyFatForm.tsx
+++ components/BodyFatForm.tsx
@@ -8,15 +8,21 @@ interface Props {
 }
 
 export default function BodyFatForm({ setBodyFats }: Props) {
+    const today = new Date().toISOString().split("T")[0];
+
     const [bodyFat, setBodyFat] = useState<number>(15);
+    const [date, setDate] = useState(today);
 
     const submitCurrentBodyFat = async () => {
         try {
             const validateBodyFat = bodyFatSchema.parse(bodyFat);
 
             if (validateBodyFat) {
+                const formattedDate = new Date(date).toISOString();
+
                 const res = await axios.post("/api/bf_measurements", {
                     bodyfat: bodyFat,
+                    createdAt: formattedDate,
                 });
 
                 if (res.status === 200) {
@@ -35,16 +41,29 @@ export default function BodyFatForm({ setBodyFats }: Props) {
     };
 
     return (
-        <form className="text-sm flex gap-4 justify-center items-center bg-gray-800 px-4 py-2 rounded-md">
-            <label htmlFor="bf">Current Body Fat (%):</label>
-            <input
-                className="w-16"
-                onChange={(e) => setBodyFat(Number(e.target.value))}
-                value={bodyFat}
-                type="number"
-                id="bf"
-                placeholder="Body Fat Percentage"
-            />
+        <form className="text-sm flex gap-4 flex-col justify-center items-center bg-gray-800 px-4 py-2 rounded-md">
+            <section className="flex gap-2 items-center">
+                <label htmlFor="weight">Date: </label>
+                <input
+                    className="w-32"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                    type="date"
+                    id="date"
+                />
+            </section>
+            <section className="flex gap-2 items-center">
+                <label htmlFor="bf">Current Body Fat (%):</label>
+                <input
+                    className="w-16"
+                    onChange={(e) => setBodyFat(Number(e.target.value))}
+                    value={bodyFat}
+                    type="number"
+                    id="bf"
+                    placeholder="Body Fat Percentage"
+                />
+            </section>
+
             <button
                 onClick={submitCurrentBodyFat}
                 type="button"
