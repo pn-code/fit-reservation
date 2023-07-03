fc340838b216b504af3366d6517d75bac3944637
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-03 13:15:42 -0700

    Created delete endpoint for weights

---
+++ app/api/weight_measurements/[weightId]/route.ts
@@ -0,0 +1,28 @@
+import { currentUser } from "@clerk/nextjs/app-beta";
+import { prisma } from "../../../../lib/client";
+import { NextResponse } from "next/server";
+
+interface Props {
+    params: { weightId: string };
+}
+
+export async function DELETE(req: Request, { params }: Props) {
+    try {
+        const user = await currentUser();
+        if (user) {
+            const { id: userId } = user;
+
+            const weightToDelete = await prisma.weightMeasurement.deleteMany({
+                where: {
+                    id: Number(params.weightId),
+                    userId: userId,
+                },
+            });
+
+            return NextResponse.json(weightToDelete);
+        }
+    } catch (error) {
+        console.log(error);
+        return NextResponse.error();
+    }
+}
