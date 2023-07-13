978c4aaebd8a83ab8328293ce6cb5e88f796f649
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-12 22:31:16 -0700

    Added target calorie goal by logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,52 @@
 "use client";
-import { Shield } from "lucide-react";
+import { Shield, Target } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import NavbarMobile from "./NavbarMobile";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Navbar = () => {
+    const [calorieGoal, setCalorieGoal] = useState(2000);
+
     const user = useUser().user;
 
     const pathname = usePathname();
     const currentPathStyles = "border-b-2 border-indigo-600";
 
+    useEffect(() => {
+        async function getCalorieGoal() {
+            try {
+                const res = await axios.get("/api/calorie_goal");
+                if (res.status === 200) {
+                    console.log(calorieGoal);
+                    setCalorieGoal(res.data.goal);
+                }
+            } catch (error) {
+                console.error();
+            }
+        }
+
+        getCalorieGoal();
+    }, []);
+
     return (
         <>
             <nav className="relative h-16 bg-[#172554] text-white py-6 px-2 sm:px-6 flex justify-between items-center w-full top-0 left-0 z-[999]">
-                <header>
+                <header className="w-full flex gap-5 items-center justify-between sm:justify-start">
                     <Link
                         className="hover:underline font-medium flex gap-2 items-center"
                         href="/"
                     >
-                        <Shield size={40} />
-                        <h1 className="text-3xl font-bold">FitHeroes</h1>
+                        <Shield size={32} />
+                        <h1 className="text-2xl lg:3xl font-bold">FitHeroes</h1>
+                    </Link>
+                    <Link href="/calculator" className="group flex flex-col gap-2 hover:underline">
+                        <span className="flex gap-1 items-center text-sm">
+                            <Target color="orange" size={20}/>
+                            Calorie Goal - {calorieGoal} kCal
+                        </span>
                     </Link>
                 </header>
 
