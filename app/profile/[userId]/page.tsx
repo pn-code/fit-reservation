7d5258ea9a74d6fd69b2edbdd24b442e96c4cf58
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-20 23:46:20 -0700

    Added calculator to Profile

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -4,65 +4,80 @@ import StripeCheckOutButton from "../../../components/StripeCheckOutButton";
 import { prisma } from "../../../lib/client";
 import { Check } from "lucide-react";
 import UpdateUserComponent from "../../../components/UpdateUserComponent";
+import Link from "next/link";
+import { SigmaSquare } from "lucide-react";
 
 interface Props {
-    params: {
-        userId: string;
-    };
+  params: {
+    userId: string;
+  };
 }
 
 export async function generateMetadata({ params }: Props) {
-    const user = await clerkClient.users.getUser(params.userId);
+  const user = await clerkClient.users.getUser(params.userId);
 
-    return {
-        title: `${user?.firstName} ${user?.lastName} | FitHeroes`,
-    };
+  return {
+    title: `${user?.firstName} ${user?.lastName} | FitHeroes`,
+  };
 }
 
 async function ProfilePage({ params }: Props) {
-    const user = await currentUser();
-    const { userId } = params;
+  const user = await currentUser();
+  const { userId } = params;
 
-    const userOnProfile = await clerkClient.users.getUser(userId);
-    const userIsSubscribed = await prisma.subscription.findFirst({
-        where: { userId: userOnProfile.id },
-    });
+  const userOnProfile = await clerkClient.users.getUser(userId);
+  const userIsSubscribed = await prisma.subscription.findFirst({
+    where: { userId: userOnProfile.id },
+  });
 
-    return (
-        <main className="w-full min-h-[calc(100vh-90px)] bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 lg:px-[20%] text-white/90">
-            <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
-                <h1 className="text-3xl">Profile</h1>
-                <SignOutButton />
-            </header>
+  return (
+    <main className="w-full min-h-[calc(100vh-90px)] bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 lg:px-[20%] text-white/90 mb-12">
+      <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
+        <h1 className="text-3xl">Profile</h1>
+        <SignOutButton />
+      </header>
 
-            {/* User Information */}
-            <section className="flex gap-4 items-center mb-5">
-                <h2 className="text-3xl font-semibold text-amber-300 flex gap-4">
-                    {`${userOnProfile?.firstName} ${userOnProfile?.lastName}`}
-                </h2>
-                {userIsSubscribed && (
-                    <span className="bg-indigo-600 rounded-full p-1 border-2 border-indigo-700 flex justify-center items-center">
-                        <Check />
-                    </span>
-                )}
-            </section>
+      {/* User Information */}
+      <section className="flex gap-4 items-center mb-5">
+        <h2 className="text-xl sm:text-3xl font-semibold text-amber-300 flex gap-4">
+          {`${userOnProfile?.firstName} ${userOnProfile?.lastName}`}
+        </h2>
+        {userIsSubscribed && (
+          <span className="bg-indigo-600 rounded-full p-0.5 border-2 border-indigo-700 flex justify-center items-center">
+            <Check />
+          </span>
+        )}
+      </section>
 
-            {/* Upgrade Profile */}
-            {!userIsSubscribed && userId === user?.id && (
-                <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
-                    <section>
-                        <h2 className="text-2xl font-semibold text-amber-300">
-                            Upgrade account to premium
-                        </h2>
-                        <p>Get a cool verified check</p>
-                    </section>
-                    <StripeCheckOutButton />
-                </section>
-            )}
+      {/* Upgrade Profile */}
+      {!userIsSubscribed && userId === user?.id && (
+        <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+          <section>
+            <h2 className="text-xl sm:text-2xl font-semibold text-amber-300">
+              Upgrade account to premium
+            </h2>
+            <p>Get a cool verified check</p>
+          </section>
+          <StripeCheckOutButton />
+        </section>
+      )}
 
-            {user?.id === userId && <UpdateUserComponent />}
-        </main>
-    );
+      <section className="flex flex-col gap-4 sm:flex-row sm:justify-between">
+        <section>
+          <h2 className="text-xl sm:text-2xl font-semibold text-amber-300">
+            Update Goals
+          </h2>
+          <p className="text-sm">Get a cool verified check</p>
+        </section>
+        <Link className="flex text-sm items-center justify-center gap-2 rounded-md px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white/90 hover:text-white sm:w-42" href="/calculator" passHref>
+          <SigmaSquare size={18}/>
+          Calculator
+        </Link>
+      </section>
+
+      {user?.id === userId && <UpdateUserComponent />}
+    </main>
+  );
 }
 
 export default ProfilePage;
