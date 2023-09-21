4316da9ddab50a6705b173a1c1ab6ccb9a7d5a09
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-21 00:28:45 -0700

    Updated loading  text size & several other pages

--- app/checkout/canceled/page.tsx
+++ app/checkout/canceled/page.tsx
@@ -4,11 +4,11 @@ export default function page() {
     return (
 		<main className="w-full h-full bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90">
             <header className="flex justify-between font-bold pb-2 border-b-2 border-b-indigo-600 items-center">
-                <h1 className="text-3xl">Checkout Canceled</h1>
+                <h1 className="text-xl sm:text-3xl">Checkout Canceled</h1>
             </header>
 
             <section className="flex flex-col gap-4">
-                <h2 className="text-2xl text-amber-400 font-semibold">Order Successfully Canceled</h2>
+                <h2 className="text-lg sm:text-2xl text-amber-400 font-semibold">Order Successfully Canceled</h2>
                 <Link
                     className="px-4 py-2 rounded-md bg-gray-500 text-white text-lg w-fit"
                     href="/"
--- app/journal/exercise/user/[userId]/loading.tsx
+++ app/journal/exercise/user/[userId]/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     return (
         <main className="w-full min-h-[calc(100vh-90px)] bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 lg:px-[20%] text-white/90">
             <header className="flex flex-col w-full gap-2">
-                <h1 className="text-3xl font-bold border-b-indigo-600 border-b-2">
+                <h1 className="text-xl sm:text-3xl font-bold border-b-indigo-600 border-b-2">
                     User&apos; Exercise Journals
                 </h1>
             </header>
