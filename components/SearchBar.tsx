612a3943ec7e9df415a51b5c6f4f81dcfff7168b
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-21 00:23:45 -0700

    Removed label and added aria label instead

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,22 +3,23 @@
 import { Search } from "lucide-react";
 
 interface Props {
-    setSearchTerm: any;
+  setSearchTerm: any;
 }
 
 export default function SearchBar({ setSearchTerm }: Props) {
-    return (
-        <section className="w-full mx-auto flex justify-center gap-2 items-center">
-            <label htmlFor="search">Search: </label>
-            <span className="relative">
-                <input
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    id="search"
-                    className="w-full sm:w-96"
-                    type="text"
-                />
-                <Search className="absolute right-2 top-1" color="black" />
-            </span>
-        </section>
-    );
+  return (
+    <section className="w-full mx-auto flex sm:justify-center gap-2 items-center">
+      <span className="relative w-full sm:w-fit">
+        <input
+          onChange={(e) => setSearchTerm(e.target.value)}
+          id="search"
+          className="w-full sm:w-96"
+          type="text"
+          placeholder="Search for Plans"
+          aria-label="Search for Plans"
+        />
+        <Search className="absolute right-2 top-1" color="black" />
+      </span>
+    </section>
+  );
 }
