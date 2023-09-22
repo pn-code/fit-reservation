6cf01f0c7b54dfb1c142a5d99156d28a91269e95
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-22 13:19:24 -0700

    Fixed login overflow on mobile

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function LoginPage() {
     return (
         <main className="w-full min-h-[calc(100vh-90px)] bg-slate-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 sm:px-10 text-white/90 justify-center items-center">
-            <section className="w-[395px] flex flex-col gap-1 bg-white rounded-md p-6 text-black">
+            <section className="w-full sm:w-[395px] flex flex-col gap-1 bg-white rounded-md p-6 text-black">
                 <h1 className="text-lg font-bold">Guest Account Credentials</h1>
                 <p className="text-indigo-600 font-[600]">
                     Try out our services.
