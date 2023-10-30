51d793e5c7ea5cf8ed0a870f7b3a8f20796054cf
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-29 21:08:00 -0700

    Updated to not create shift when page is loaded

--- app/journal/loading.tsx
+++ app/journal/loading.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Spinner from "../../components/Spinner";
+import Link from "next/link";
 
 export default function Loading() {
     return (
         <main className="w-full min-h-[calc(100vh-90px)] bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 lg:px-[20%] text-white/90">
-            <header className="flex flex-col w-full gap-2">
-                <h1 className="text-xl sm:text-3xl font-bold border-b-indigo-600 border-b-2">
-                    Journal
-                </h1>
+            <header className="w-full flex justify-between border-b-2 border-b-indigo-600">
+                <section className="w-full flex justify-between font-bold pb-2 items-center h-full">
+                    <h1 className="text-xl sm:text-3xl">Journal</h1>
+                    <div>
+                        <Link
+                            className="text-sm font-semibold p-2 underline hover:text-slate-300"
+                            href={`/journal/nutrition/user/`}
+                        >
+                            All Nutrition
+                        </Link>
+                        <Link
+                            className="text-sm font-semibold p-2 underline hover:text-slate-300"
+                            href={`/journal/exercise/user/`}
+                        >
+                            All Exercises
+                        </Link>
+                    </div>
+                </section>
             </header>
 
             <section className="w-full h-full flex justify-center mt-40">
--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -14,7 +14,7 @@ const JournalPage = async () => {
 
     return (
         <main className="w-full min-h-[calc(100vh-100px)] pb-20 overflow-y-auto bg-gray-900 py-6 rounded-md flex flex-col gap-4 shadow-md px-2 lg:px-[20%] text-white/90">
-            <header className="w-full flex justify-between lg:border-b-2 lg:border-b-indigo-600">
+            <header className="w-full flex justify-between border-b-2 border-b-indigo-600">
                 <section className="w-full flex justify-between font-bold pb-2 items-center h-full">
                     <h1 className="text-xl sm:text-3xl">Journal</h1>
                     <div>
