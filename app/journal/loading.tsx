0dc1fa5dd24d90b7170c620acab7fdcf02d5231d
Philip Nguyen <philipnguyen953@gmail.com>
2024-01-12 17:32:04 -0800

    Updated Journal header

--- app/journal/loading.tsx
+++ app/journal/loading.tsx
@@ -3,18 +3,17 @@ import Spinner from "@/components/Spinner";
 export default function Loading() {
     return (
         <main className="w-full min-h-[calc(100vh-100px)] pb-20 overflow-y-auto py-6 rounded-md flex flex-col gap-4 shadow-md px-4 md:px-[4%]">
-            <header className="w-full flex justify-between border-b-2 border-b-primary">
-                <section className="w-full flex justify-between font-bold pb-2 items-center h-full">
+            <header className="bg-white border border-primary p-4 rounded-sm shadow-md flex justify-between">
+                <div className="flex flex-col">
                     <h1>Journal</h1>
-                    <div className="flex gap-2 ">
-                        <button className="btn btn--primary">
-                            All Nutrition
-                        </button>
-                        <button className="btn btn--primary">
-                            All Exercises
-                        </button>
-                    </div>
-                </section>
+                    <p className="tracking-tighter text-secondary">
+                        Keep track of progress
+                    </p>
+                </div>
+                <div className="flex gap-2">
+                    <button className="btn btn--primary">All Nutrition</button>
+                    <button className="btn btn--primary">All Exercises</button>
+                </div>
             </header>
 
             <section className="w-full h-full flex justify-center mt-40">
--- app/journal/page.tsx
+++ app/journal/page.tsx
@@ -5,39 +5,42 @@ import JournalSelector from "@/components/journals/JournalSelector";
 import { currentUser } from "@clerk/nextjs";
 
 export const metadata = {
-  title: "Journal | FitHeroes",
+    title: "Journal | FitHeroes",
 };
 
 const JournalPage = async () => {
-  const user = await currentUser();
+    const user = await currentUser();
 
-  if (!user) return redirect("/");
+    if (!user) return redirect("/");
 
-  return (
-    <main className="w-full min-h-[calc(100vh-100px)] pb-20 overflow-y-auto py-6 rounded-md flex flex-col gap-4 shadow-md px-4 md:px-[4%]">
-      <header className="w-full flex justify-between border-b-2 border-b-primary">
-        <section className="w-full flex justify-between font-bold pb-2 items-center h-full">
-          <h1>Journal</h1>
-          <div className="flex gap-2 ">
-            <Link
-              className="btn btn--primary"
-              href={`/journal/nutrition/user/${user.id}`}
-            >
-              All Nutrition
-            </Link>
-            <Link
-              className="btn btn--primary"
-              href={`/journal/exercise/user/${user.id}`}
-            >
-              All Exercises
-            </Link>
-          </div>
-        </section>
-      </header>
+    return (
+        <main className="w-full min-h-[calc(100vh-100px)] pb-20 overflow-y-auto py-6 rounded-md flex flex-col gap-4 shadow-md px-4 md:px-[4%]">
+            <header className="bg-white border border-primary p-4 rounded-sm shadow-md flex justify-between">
+                <div className="flex flex-col">
+                    <h1>Journal</h1>
+                    <p className="tracking-tighter text-secondary">
+                        Keep track of progress
+                    </p>
+                </div>
+                <div className="flex gap-2">
+                    <Link
+                        className="btn btn--primary"
+                        href={`/journal/nutrition/user/${user.id}`}
+                    >
+                        All Nutrition
+                    </Link>
+                    <Link
+                        className="btn btn--primary"
+                        href={`/journal/exercise/user/${user.id}`}
+                    >
+                        All Exercises
+                    </Link>
+                </div>
+            </header>
 
-      <JournalSelector />
-    </main>
-  );
+            <JournalSelector />
+        </main>
+    );
 };
 
 export default JournalPage;
