a8632c79baea34f1431131a028771f2f5bae3408
Philip Nguyen <philipnguyen953@gmail.com>
2024-01-15 19:10:19 -0800

    Updated loading pages

--- app/journal/loading.tsx
+++ app/journal/loading.tsx
@@ -3,7 +3,7 @@ import Spinner from "@/components/Spinner";
 export default function Loading() {
     return (
         <main className="w-full min-h-[calc(100vh-100px)] pb-20 overflow-y-auto py-6 rounded-md flex flex-col gap-4 shadow-md px-4 md:px-[4%]">
-            <header className="bg-white border border-primary p-4 rounded-sm shadow-md flex justify-between">
+            <header className="bg-white border border-primary p-4 rounded-sm shadow-md flex justify-between flex-col md:flex-row">
                 <div className="flex flex-col">
                     <h1>Journal</h1>
                     <p className="tracking-tighter text-secondary">
@@ -12,7 +12,7 @@ export default function Loading() {
                 </div>
                 <div className="flex gap-2">
                     <button className="btn btn--primary">All Nutrition</button>
-                    <button className="btn btn--primary">All Exercises</button>
+                    <button className="btn btn--secondary">All Exercises</button>
                 </div>
             </header>
 
