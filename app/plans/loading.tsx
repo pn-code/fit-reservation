40cb58e685f56a2982e5d0bfaec6024d749ac470
Philip Nguyen <philipnguyen953@gmail.com>
2024-01-25 11:41:49 -0800

    Updated old loading pages to look similar to parent page

---
+++ app/plans/loading.tsx
@@ -0,0 +1,18 @@
+import Spinner from "@/components/Spinner";
+
+export default function Loading() {
+    return (
+        <main className="w-full min-h-[calc(100vh-64px)] py-6 mb-12 md:mb-0 flex flex-col px-4 md:px-[4%] relative">
+            <header className="bg-white border border-primary p-4 rounded-sm shadow-md">
+                <h1 className="font-bold">User's Plans</h1>
+                <p className="text-secondary tracking-tighter">
+                    View created and/or saved plans
+                </p>
+            </header>
+
+            <section className="w-full h-full flex justify-center mt-40">
+                <Spinner size={16} />
+            </section>
+        </main>
+    );
+}
